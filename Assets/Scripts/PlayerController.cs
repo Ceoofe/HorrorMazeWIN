51fe5e4bc2c705d7a5725086d39df5625e2a1465
Ceoofe <Avillarruel26@aoiths.org>
2026-02-12 02:14:46 -0500

    Updated Text
    Fixed Text Objectives Logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -112,14 +112,27 @@ public class PlayerController : MonoBehaviour
     IEnumerator NewObjective(TMP_Text oldText, string newObj, int objNum) // Replace text
     {
         int index = oldText.text.LastIndexOf("•");
+
         if (objNum == 1)
         {
-            string oldObj = oldText.text.Substring(0, index);
+            string oldObj;
             string remain = oldText.text.Substring(index);
+            if (index == 0)
+            {
+                oldObj = oldText.text.Substring(index);
+                oldObj = "<s>" + oldObj + "</s>";
+                oldText.text = oldObj;
+                yield return new WaitForSeconds(2f);
+                oldText.text = newObj;
+                yield break;
+            }
+            else
+            {
+                oldObj = oldText.text.Substring(0, index);
 
-            oldObj = "<s>" + oldObj + "</s>";
-
-            oldText.text = oldObj + remain;
+                oldObj = "<s>" + oldObj + "</s>";
+                oldText.text = oldObj + remain;
+            }
 
             yield return new WaitForSeconds(2f);
 
@@ -127,12 +140,20 @@ public class PlayerController : MonoBehaviour
         }
         else if (objNum == 2)
         {
-            string remain = oldText.text.Substring(0, index);
-            string oldObj = oldText.text.Substring(index);
-
-            oldObj = "<s>" + oldObj + "</s>";
-
-            oldText.text = remain + oldObj;
+            string remain;
+            string oldObj;
+            if (index == 0)
+            {
+                remain = oldText.text.Substring(index);
+                oldText.text = remain + newObj;
+            }
+            else
+            {
+                remain = oldText.text.Substring(0, index);
+                oldObj = oldText.text.Substring(index);
+                oldObj = "<s>" + oldObj + "</s>";
+                oldText.text = remain + oldObj;
+            }
 
             yield return new WaitForSeconds(2f);
 
